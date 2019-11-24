f5ebe8353caf322215343ea248bf37d2cab44ecb
neilprado <neiljohn17@gmail.com>
2019-11-24 13:22:29 -0300

    Boilerplate do projeto

---
+++ autoEvents/src/test/java/br/edu/ifpb/deps/autoEvents/AutoEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.deps.autoEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
