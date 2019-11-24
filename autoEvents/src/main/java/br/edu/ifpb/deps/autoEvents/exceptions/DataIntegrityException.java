ef241c948be99d8d455c5e70fc8f3c02684d6b82
neilprado <neiljohn17@gmail.com>
2019-11-24 13:21:17 -0300

    Camada exceptions em desenvolvimento

---
+++ autoEvents/src/main/java/br/edu/ifpb/deps/autoEvents/exceptions/DataIntegrityException.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.deps.autoEvents.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+    private static final long serialVerionUID = 1L;
+
+    public DataIntegrityException(String message) {
+        super(message);
+    }
+
+    public DataIntegrityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
